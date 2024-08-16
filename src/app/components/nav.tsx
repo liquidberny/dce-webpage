a813472c25f44b4405cd009f58324f60c3f63ae1
adiosulises <3adiosulises@gmail.com>
2024-08-15 20:01:33 -0700

    navabar full

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -4,10 +4,11 @@ import Image from 'next/image'
 
 function Nav() {
   return (
-    <div className="flex justify-between bg-white border-black text-black p-6">
+    <div className="flex items-center justify-between bg-white border-black text-black p-6 sticky top-0">
         <div className="logo">
             <Link href='/'>
                 <Image 
+                    className='nav-logo'
                     src='/dce-logo-black.svg'
                     width={80}
                     height={80}
@@ -15,7 +16,7 @@ function Nav() {
                 />
             </Link>
         </div>
-        <div className="links flex gap-4">
+        <div className="links flex gap-6">
             <Link href='/eventos'>Archivo</Link>
             <Link href='/merch'>Tienda</Link>
             <Link href='/about'>Nosotres</Link>
