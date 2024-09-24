f56f5bfef3fb3e4fd947af3fad77e0cfa546a398
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-09-23 22:26:09 -0700

    pagina eventos responsiva

--- src/app/components/Home/EventCard.tsx
+++ src/app/components/Home/EventCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -7,33 +7,34 @@ interface EventCardProps {
     title: string;
     ubi: string;
     desc: string;
-    date: string
+    date: string;
 }
 
 const EventCard: React.FC<EventCardProps> = ({ img, title, ubi, desc, date }) => {
     return (
-        <div className='border-solid border-2 border-black p-6 bg-white text-black flex flex-col justify-between'>
+        <div className='border-2 border-black p-6 bg-white text-black flex flex-col justify-between shadow-lg'>
             <div>
                 <Image
                     src={img}
                     width={500}
                     height={500}
                     alt='event img'
-                    className='border-solid border-2 border-black mb-2'
+                    className='border-2 border-black mb-2'
                 />
-                <div className="flex justify-between text-xs">
+                <div className="flex justify-between text-xs mb-2">
                     <p>{date}</p>
                     <h3 className='uppercase'>@{ubi}</h3>
                 </div>
-                
-                <h2 className='font-bold uppercase text-2xl'>{title}</h2>
-                <h4>{desc}</h4>
+                <h2 className='font-bold uppercase text-2xl mb-2'>{title}</h2>
+                <p>{desc}</p>
             </div>
-            <div >
-                <Link className="flex bg-black text-white p-4 uppercase justify-center mt-4" href='/eventos/evento'>Ver M&aacute;s</Link>
+            <div>
+                <Link className="flex bg-black text-white p-4 uppercase justify-center mt-4" href='/eventos/evento'>
+                    Ver Más
+                </Link>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default EventCard
+export default EventCard;
--- src/app/eventos/page.tsx
+++ src/app/eventos/page.tsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import EventCard from '../components/Home/EventCard'
+import React from 'react';
+import EventCard from '../components/Home/EventCard';
 
 function Eventos() {
   return (
-    <div className='h-screen py-2'>
-      <div className="container mx-auto grid grid-cols-3 gap-2">
+    <div className='py-2'>
+      <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         <EventCard
-          img=''
+          img='/images/event1.jpg'
           title='Humano 25'
           ubi='Parque la Ruina'
-          desc='Festival de m&uacute;sica mexicano'
+          desc='Festival de música mexicano'
           date='22.03.25'
         />
         <EventCard
-          img=''
+          img='/images/event2.jpg'
           title='Valgur + Estrella del Sol'
           ubi='Parroquia Nuestra Señora del Carmen'
-          desc='Valgur y Estrella del Sol presentar&aacute;n un set en la parroquia "Nuestra Señora del Carmen".'
+          desc='Valgur y Estrella del Sol presentarán un set en la parroquia "Nuestra Señora del Carmen".'
           date='22.11.24'
         />
         <EventCard
-          img=''
+          img='/images/event3.jpg'
           title='Valgur + Estrella del Sol'
           ubi='Parroquia Nuestra Señora del Carmen'
-          desc='Valgur y Estrella del Sol presentar&aacute;n un set en la parroquia "Nuestra Señora del Carmen".'
+          desc='Valgur y Estrella del Sol presentarán un set en la parroquia "Nuestra Señora del Carmen".'
           date='22.11.24'
         />
         <EventCard
-          img=''
+          img='/images/event4.jpg'
           title='Valgur + Estrella del Sol'
           ubi='Parroquia Nuestra Señora del Carmen'
-          desc='Valgur y Estrella del Sol presentar&aacute;n un set en la parroquia "Nuestra Señora del Carmen".'
+          desc='Valgur y Estrella del Sol presentarán un set en la parroquia "Nuestra Señora del Carmen".'
           date='22.11.24'
         />
       </div>
     </div>
-  )
+  );
 }
 
-export default Eventos
+export default Eventos;
