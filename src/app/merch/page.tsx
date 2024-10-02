dbdaf5fe628de141e5398aadd71d1b0be0648da3
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-10-01 21:17:57 -0700

    renovada las merch cards

--- src/app/merch/page.tsx
+++ src/app/merch/page.tsx
@@ -1,37 +1,30 @@
 import React from 'react';
-import EventCard from '../components/Home/EventCard';
+import MerchCard from '../components/MerchCard'
 
 function Merch() {
   return (
     <div className='py-2'>
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        <EventCard
-          img='/images/event1.jpg'
-          title='Humano 25'
-          ubi='Parque la Ruina'
-          desc='Festival de música mexicano'
-          date='22.03.25'
+        <MerchCard
+          img="/images/camiseta.png"
+          name="Camiseta Logo DCE"
+          price="$25.00"
+          sizes={['S', 'M', 'L', 'XL']}
+          desc="Camiseta con el logo oficial del evento DCE. Algodón 100% de alta calidad."
         />
-        <EventCard
-          img='/images/event2.jpg'
-          title='Valgur + Estrella del Sol'
-          ubi='Parroquia Nuestra Señora del Carmen'
-          desc='Valgur y Estrella del Sol presentarán un set en la parroquia "Nuestra Señora del Carmen".'
-          date='22.11.24'
+        <MerchCard
+          img="/images/gorra.png"
+          name="Gorra Edición Especial"
+          price="$15.00"
+          sizes={['Única']}
+          desc="Gorra ajustable edición especial del evento. Disponible en color negro y blanco."
         />
-        <EventCard
-          img='/images/event3.jpg'
-          title='Valgur + Estrella del Sol'
-          ubi='Parroquia Nuestra Señora del Carmen'
-          desc='Valgur y Estrella del Sol presentarán un set en la parroquia "Nuestra Señora del Carmen".'
-          date='22.11.24'
-        />
-        <EventCard
-          img='/images/event4.jpg'
-          title='Valgur + Estrella del Sol'
-          ubi='Parroquia Nuestra Señora del Carmen'
-          desc='Valgur y Estrella del Sol presentarán un set en la parroquia "Nuestra Señora del Carmen".'
-          date='22.11.24'
+        <MerchCard
+          img="/images/hoodie.png"
+          name="Hoodie Logo DCE"
+          price="$40.00"
+          sizes={['M', 'L', 'XL']}
+          desc="Hoodie con el logo oficial del evento DCE. Mantente abrigado con estilo."
         />
       </div>
     </div>
